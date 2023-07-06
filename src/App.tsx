431d86875a703666c453a493758d2c7da0da13ef
Paulo Vitor <paulovitorvaztst@gmail.com>
2023-07-06 11:48:35 -0300

    feat: inserindo "feat"

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
-import { Task } from './components/Task'
+import { useState } from 'react'
 
+import { Task } from './components/Task'
 
 import './global.css'
 import styles from './App.module.css'
 
 import { Input } from '@chakra-ui/react'
+import { Item } from './types/Item'
 
 
 export function  App() {
-
+  const [list, useList] = useState<Item[]>([
+    { id: 1, name: 'Comprar pão', done: false}
+  ])
   return (
     <div  className={styles.container}>
       <div className={styles.box}>

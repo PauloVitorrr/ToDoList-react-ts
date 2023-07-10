71c1d400c8994e1d753ec994fee85533e288a0cc
Paulo Vitor <paulovitorvaztst@gmail.com>
2023-07-10 11:00:04 -0300

    feat: inserindo valores task

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,58 @@
-import { useState } from 'react'
+import { ChangeEvent, FormEvent, useState } from 'react'
 
 import { Task } from './components/Task'
+import { Item } from './types/Item'
 
 import './global.css'
 import styles from './App.module.css'
 
 import { Input } from '@chakra-ui/react'
-import { Item } from './types/Item'
+import { Plus } from 'phosphor-react'
 
 
 export function  App() {
-  const [list, useList] = useState<Item[]>([
+  const [list, setList] = useState<Item[]>([
     { id: 1, name: 'Comprar pão', done: false},
     { id: 2, name: 'Fazer atividade', done: false}
   ])
+
+  const [inputText, setInputText] = useState('')
+
+    function handleAddTask (event: FormEvent){
+    event.preventDefault();
+
+    const newList = [...list];
+    newList.push({
+      id: list.length +1,
+      name: inputText,
+      done: false,
+    });
+
+    setList(newList);
+    setInputText('');
+  }
+
+  function handleNewTaskChange(event: ChangeEvent<HTMLInputElement>){
+    event.target.setCustomValidity('')
+
+    setInputText(event.target.value)
+  }
+
   return (
     <div  className={styles.container}>
       <div className={styles.box}>
-        <form>
+
+        <form onSubmit={handleAddTask} className={styles.containerInputAdd}>
           <Input 
             placeholder='Digite sua task'
+            value={inputText}
+            onChange={handleNewTaskChange}
           />
+          <button type="submit" className={styles.buttonAddTask}>
+            <Plus size={20}/>
+          </button>
         </form>
+
         <div>
           {list.map(listt =>{
             return(
